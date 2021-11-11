56cec56f017ad7fd347438c54b2f1a56b7c2b57a
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2021-11-10 20:18:13 -0700

    added script link for jQuery, created array of objects for quiz questions, started on the function that will run when the start button is clicked.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,70 @@
 //Variable Declarations
-var startBtn = document.querySelector(".btn-start");
+var startBtn = $(".btn-start");
 
-var questionArea = document.querySelector(".question-area");
+var questionArea = $(".question-area");
 
-var introText = document.querySelector("#intro")
+var introText = $("#intro");
 
+//Array of objects to serve as the source for questions
+var questions = [ {
+    question:"Which American author is most well-known for the Earthsea Cycle series?", 
+    ans1:"Rebecca Roanhorse",
+    ans2:"Ursula K. LeGuin",
+    ans3:"Alyssa Wong",
+    ans4:"Ursula Vernon"
+    },
 
+    {
+        question:"Which American author became the first author to win the Hugo award for best novel for three consecutive years for the Broken Earth series?",
+        ans1:"Nnedi Okorafor",
+        ans2:"Tasha Suri",
+        ans3:"N.K. Jemisin",
+        ans4:"Robin Hobb"
+    },
 
-//When the user clicks on the start button, a question appears on the screen. Will need an array of question objects. 
+    {
+      question:"Which British author is the self-proclaimed Lord Grimdark and well-known for the flawed characters and dark humor in the First Law and Age of Madness series?",
+        ans1:"Joe Abercrombie",
+        ans2:"Mark Lawrence",
+        ans3:"George R.R. Martin",
+        ans4:"Patrick Rothfuss"  
+    },
 
+        {
+    question:"Which British author is known as the father of modern fantasy?",
+        ans1:"Richard K. Morgan",
+        ans2:"Ed McDonald",
+        ans3:"Robin Hobb",
+        ans4:"J.R.R. Tolkien"
+        }
+];
 
+//Picks a random question object from the questions array
+var randomQuestion = $(questions[Math.floor(Math.random() * questions.length)]);
+console.log(randomQuestion);
 
-startBtn.addEventListener("click", startQuestion);
+//displays question when start button is clicked
+function startQuestion(event) {
+    event.preventDefault;
+    //Remove paragraph element, h1 element
+    $("#intro").hide();
+    $("#title").hide();
+    $(".btn-start").hide();
+    //Select an object from the questions array
+    
+    //Insert object into an h2 element, append to the question area section
+    var newQuestion=$("<h2>").text(randomQuestion);
+    $(".question-area").append(newQuestion);
+    //Create a ul and append to the question-area section
+    var questionAnswerList = $("<ul>");
+    $(".question-area").append(questionAnswerList);
+    //Display the values for the a1, 2, 3, 4 keys
+}
 
-
+startBtn.click(startQuestion);
 
 //Once quiz starts, timer should display in the top right hand corner and show much time is left. Use setInterval and clearInterval for timer. 
+
 
 //When the question is answered, it progresses to another question
 //If the question is answered incorrectly, time is removed from the clock
